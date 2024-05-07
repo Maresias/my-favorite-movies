ae72b9c7915abe985060f8bc94964cd5edc4815c
AlexandreRCS <ale.maresias@gmail.com>
2024-05-06 22:50:26 -0300

    Update
    deleting unnecessary files

--- src/App.jsx
+++ src/App.jsx
@@ -16,9 +16,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Alexandre</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + -3)}>
           count is {count}
         </button>
         <p>
