cea8087746739e4c7be3804b9487783912fc62f6
AlexandreRCS <ale.maresias@gmail.com>
2024-05-07 21:47:15 -0300

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 
 
+import GlobalStyles from './pages/styles/global'
 import theme from './pages/styles/theme'
 
 
@@ -13,6 +14,7 @@ import {Details} from './pages/Details'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider  theme={ theme }>
+      <GlobalStyles/>
       <Details />
     </ThemeProvider>
   </React.StrictMode>,
