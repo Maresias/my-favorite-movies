33203123ea6ef837ee2f5450432af393797ce6aa
AlexandreRCS <ale.maresias@gmail.com>
2024-05-07 21:23:02 -0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
+
+
+import theme from './pages/styles/theme'
+
+
 import {Details} from './pages/Details'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Details />
+    <ThemeProvider  theme={ theme }>
+      <Details />
+    </ThemeProvider>
   </React.StrictMode>,
 )
