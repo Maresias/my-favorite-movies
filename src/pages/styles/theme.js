cd316495eb28c0ee3a87e2ea88ea7a3578f13421
AlexandreRCS <ale.maresias@gmail.com>
2024-05-07 21:10:25 -0300

    Create theme.js

---
+++ src/pages/styles/theme.js
@@ -0,0 +1,13 @@
+export default {
+    COLORS:{
+        BACKGROUND_900: "#262529",
+        BACKGROUND_800: "#312E38",
+        BACKGROUND_700: "#948F99",
+
+        PINK: "#FF859B",
+        WHITE: "F4EDE8",
+
+        GRAY_200: "#948F99",
+        GRAY_100: "#E5E5E5"
+    }
+}
