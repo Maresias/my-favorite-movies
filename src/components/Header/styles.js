26911f1af3095832264457aa619206ee2009cc19
AlexandreRCS <ale.maresias@gmail.com>
2024-05-08 21:38:34 -0300

    Update

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const Container = styled.header`
     border-bottom: 1px solid white;
     height: 11rem;
+    
 `
 
 
--- src/pages/styles/global.js
+++ src/pages/styles/global.js
@@ -7,9 +7,15 @@ export default createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+
+        -webkit-font-smoothings:grayscale;
+        -moz-font-smoothings:antialiased;
     }
+
+    
 
     body {
         background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+        font-family: "Roboto", sans-serif;
     }
 `
