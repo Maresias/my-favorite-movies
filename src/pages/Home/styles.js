db072a2b77f72066a430a7163cebf038854cf6d9
AlexandreRCS <ale.maresias@gmail.com>
2024-05-10 21:29:42 -0300

    Update styles.js

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,15 +1,16 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-
-
+    width: 100%;
+    height: 100vh;
+    overflow-y: hidden;
 `
 
 export const Box = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
-    height: 10rem;
+    height: 7rem;
     max-width: 900px;
     margin: 0 auto;
 `
