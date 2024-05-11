ee65000264f039c0e2ff33a4f633349cb9e63c87
AlexandreRCS <ale.maresias@gmail.com>
2024-05-10 21:23:01 -0300

    Update

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Content } from './styles'
+import { Box, Container, Content } from './styles'
 
 
 import { Header } from '../../components/Header'
@@ -7,6 +7,11 @@ export function Home(){
     return (
         <Container>
             <Header/>
+            <Box>
+                <h2>Meus Filmes</h2>
+
+                botao
+            </Box>
             <Content>
 
             </Content>
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -5,9 +5,20 @@ export const Container = styled.div`
 
 `
 
+export const Box = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 10rem;
+    max-width: 900px;
+    margin: 0 auto;
+`
+
 export const Content = styled.div`
     background-color: gray;
     max-width:  900px;
     margin: 0 auto;
     height: 700px;
 `
+
+
