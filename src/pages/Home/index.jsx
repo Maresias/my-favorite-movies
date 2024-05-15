bc4a3871bd97ee1b64be2256beb1bbb61a5309c8
AlexandreRCS <ale.maresias@gmail.com>
2024-05-14 22:53:48 -0300

    Update index.jsx

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,7 +3,7 @@ import { FiPlus } from 'react-icons/fi'
 
 
 import { Header } from '../../components/Header'
-import { Section } from '../../components/Section'
+import { MyReviews } from '../../components/MyReviews'
 import { Tag } from '../../components/Tag'
 
 export function Home(){
@@ -18,7 +18,7 @@ export function Home(){
                 </NewMovie>
             </Box>
             <Content>
-                <Section title={"Homen Aranha"}>
+                <MyReviews title={"Homen Aranha"}>
                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id delectus nemo debitis hic suscipit dolor veniam impedit deserunt tenetur perspiciatis veritatis consequatur possimus doloribus porro totam, iure dolore corrupti aperiam.</p>
                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta quasi quisquam deleniti quidem, magni enim expedita obcaecati recusandae ea voluptates laudantium debitis necessitatibus quos voluptate adipisci excepturi, accusamus dolorum temporibus?</p>
                    <Frame>
@@ -26,7 +26,7 @@ export function Home(){
                      <Tag title="Acão"/>
                      <Tag title="Herói"/>
                    </Frame>
-                </Section>
+                </MyReviews>
             </Content>
         </Container>
     )
