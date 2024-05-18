cf9e5d81b0cff68ddd959cd56e32b1051c6b0734
AlexandreRCS <ale.maresias@gmail.com>
2024-05-17 21:39:01 -0300

    Update

--- src/pages/Perfil/styles.js
+++ src/pages/Perfil/styles.js
@@ -8,4 +8,9 @@ export const Container = styled.div`
 export const Avatar = styled.div`
     width: 100%;
     height: 10rem;
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_600};
+`
+
+export const Form = styled.form`
+    
 `
--- src/pages/styles/theme.js
+++ src/pages/styles/theme.js
@@ -3,7 +3,7 @@ export default {
         BACKGROUND_900: "#262529",
         BACKGROUND_800: "#312E38",
         BACKGROUND_700: "#FF859B",
-        BACKGROUND_600: RGBA(255,133,155,0.16),
+        BACKGROUND_600: "rgba(255,133,155,0.16)",
 
         PINK: "#FF859B",
         WHITE: "#F4EDE8",
