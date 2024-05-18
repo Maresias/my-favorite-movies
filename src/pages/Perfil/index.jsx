c3c7161cf5b3c2ca959f70b0ccd4a18c2183be9d
AlexandreRCS <ale.maresias@gmail.com>
2024-05-17 21:45:04 -0300

    Update index.jsx

--- src/pages/Perfil/index.jsx
+++ src/pages/Perfil/index.jsx
@@ -1,13 +1,19 @@
 import { Container, Avatar, Form} from './styles'
 
+import { FiArrowLeft } from 'react-icons/fi'
+import { ButtonText } from '../../components/ButtonText'
+
 export function Perfil (){
     return (
         <Container>
             <Avatar>
-
+                <div>
+                    <FiArrowLeft/>
+                    <ButtonText title={"Voltar"}/>
+                </div>
             </Avatar>
                 <Form>
-                    
+
                 </Form>
         </Container>
     )
