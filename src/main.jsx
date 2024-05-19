6a34e1a39abf917012022c2e7d156c890eca3fce
AlexandreRCS <ale.maresias@gmail.com>
2024-05-18 21:22:36 -0300

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import GlobalStyles from './pages/styles/global'
 import theme from './pages/styles/theme'
 
 
-import {Profile} from './pages/Profile'
+import {SingIn} from './pages/SignIn'
 
 
 
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider  theme={ theme }>
       <GlobalStyles/>
-      <Profile />
+      <SingIn />
     </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,9 @@
+import { Container } from './styles'
+
+export function SingIn(){
+    return (
+        <Container>
+            
+        </Container>
+    )
+}
