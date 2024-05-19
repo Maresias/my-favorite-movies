70b74d74f7a588eb766a3169cfb9510c4655c857
AlexandreRCS <ale.maresias@gmail.com>
2024-05-18 23:10:40 -0300

    Update

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,4 +1,5 @@
 import { Container, Form, Brand } from './styles'
+import { FiLock, FiMail } from 'react-icons/fi'
 
 import  backgroundImg from '../../assets/backgrounImg.png'
 
@@ -11,16 +12,16 @@ export function SingIn(){
     return (
         <Container>
             <div>
-                <Brand>
-                    <h1>RocketMovies</h1>
-                    <p>Aplicação para acompanhar tudo que assistir </p>
-                </Brand>
                 <Form>
+                    <Brand>
+                        <h1>RocketMovies</h1>
+                        <p>Aplicação para acompanhar tudo que assistir </p>
+                    </Brand>
                     <legend>
                         Faça seu login
                     </legend>
-                    <Input/>
-                    <Input/>
+                    <Input icon={FiMail} type="text" placeholder="E-Mail"/>
+                    <Input icon={FiLock} type="password" placeholder="Senha"/>
                     <Button title={"Entra"}/>
                     <a href="#"><ButtonText title="Criar Conta"/></a>
                 </Form>
--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -13,11 +13,25 @@ export const Container = styled.div`
 
     > div {
         width: 50%;
+        display: grid;
+        height: 100vh;
+        justify-content: center;
+        align-items: center;
     }
 `
 export const Form = styled.form`
-   
+    text-align: center;
+    margin-top: -50px;
+    width: 400px;
 `
 export const Brand = styled.div`
+    h1 {
+        color: ${({ theme }) => theme.COLORS.PINK};
+        font-size: 3.4rem;
+        
+    }
 
+    p {
+        color: ${({ theme }) => theme.COLORS.GRAY_200};
+    }
 `
