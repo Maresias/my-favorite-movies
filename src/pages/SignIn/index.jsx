ab49bbfba7dfdcb2a503fdc2348ad2057c0eafc7
AlexandreRCS <ale.maresias@gmail.com>
2024-05-18 21:32:46 -0300

    Update

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,9 +1,14 @@
-import { Container } from './styles'
+import { Container, Form } from './styles'
+
+import  backgroundImg from '../../assets/backgrounImg.png'
 
 export function SingIn(){
     return (
         <Container>
-            
+            <Form>
+                    <h1>MAresias</h1>
+            </Form>
+            <img src={backgroundImg} alt="Imagem de uma sala de cinema" />
         </Container>
     )
 }
--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -1,5 +1,16 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-    
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: space-between;
+
+    > img {
+        width: 50%;
+        opacity: 0.4;
+    }
+`
+export const Form = styled.form`
+    width: 50%;
 `
