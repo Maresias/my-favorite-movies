fbfcbc4fe6e0b611fff915f7d88550369c2e8a31
AlexandreRCS <ale.maresias@gmail.com>
2024-05-19 21:58:33 -0300

    Update app.routes.jsx

---
+++ src/routes/app.routes.jsx
@@ -0,0 +1,16 @@
+import { Router, Route } from "react-router-dom";
+
+import { Home } from '../pages/Home'
+import { Profile } from '../pages/Profile'
+import { CreateMovie } from '../pages/CreateMovie'
+import { MoviePreview } from '../pages/MoviePreview'
+
+
+export function appRoutes(){
+    <Router>
+        <Route path="/" element={ <Home/> }/>
+        <Route path="/profile" element={ <Profile/> } />
+        <Route path="/createmovie" element={ <CreateMovie/> }/>
+        <Route path="/MoviePreview" element={ <MoviePreview/> } />
+    </Router>
+}
