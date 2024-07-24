0810471d09900bbf81d33f088b25a265b55205bb
AlexandreRCS <ale.maresias@gmail.com>
2024-07-23 22:30:33 -0300

    Update auth.jsx

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,16 +1,22 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 
 import { api } from "../services/api"
 
 export const AuthContext = createContext({})
 
 function AuthProvider({ children }) {
+    
+    const [ data, setData ] = useState({})
 
     async function signIn({email, password}){
 
         try{
             const response =  await api.post("/sessions", {email, password})
-            console.log(response)
+            const {user, token } = response.data
+            
+            api.defaults.headers['Authorization'] = `Bearer ${token}`
+
+            setData({user, token})
         }catch(error){
             if(error.response){
                 alert(error.response.data.message)
@@ -21,7 +27,7 @@ function AuthProvider({ children }) {
     }
 
     return (
-        <AuthContext.Provider value={{signIn}}>
+        <AuthContext.Provider value={{ signIn, user: data.user}}>
             {children}
         </AuthContext.Provider>
     )
