ba4164402459cfc536660edd9a4cbd7ad9c9a471
AlexandreRCS <ale.maresias@gmail.com>
2024-07-25 22:51:58 -0300

    Update index.jsx

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,15 +1,23 @@
+import { useState } from 'react'
 import { Container, Avatar, Form} from './styles'
 import { Link } from 'react-router-dom'
 
 import { FiArrowLeft, FiMail, FiUser, FiLock, FiCamera } from 'react-icons/fi'
 
-
+import {useAuth} from '../../hooks/auth'
 
 import { ButtonText } from '../../components/ButtonText'
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
 
 export function Profile (){
+
+    const { user } = useAuth()
+
+    const [ name, setName ] = useState(user.name)
+    const [ email, setEmail ] = useState(user.email)
+    const [ passwordOld, setPassworldOld ] = useState()
+    const [ passworldNew, setPassworldNew ] = useState()
     return (
         <Container>
             <header>
@@ -31,7 +39,10 @@ export function Profile (){
                     </label>
                     </Avatar>
                
-                        <Input icon={FiUser} placeholder="Alexandre Correia" type="text"/>
+                        <Input 
+                            icon={FiUser} 
+                            placeholder="Alexandre Correia" 
+                            type="text"/>
                         <Input icon={FiMail} placeholder="Email" type="text"/>
                         <Input icon={FiLock} placeholder="Senha Atual" type="password"/>
                         <Input icon={FiLock} placeholder="Nova Senha" type="password"/>
