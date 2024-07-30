f498cfa43a056520587eda1a72024809a66bf0e2
AlexandreRCS <ale.maresias@gmail.com>
2024-07-29 23:45:00 -0300

    Update

--- src/pages/CreateMovie/index.jsx
+++ src/pages/CreateMovie/index.jsx
@@ -1,7 +1,8 @@
-import { Container } from "./styles";
+import { Container, Form } from "./styles";
 
 import { Header } from "../../components/Header";
 import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
 
 
 
@@ -9,6 +10,14 @@ export function CreateMovie ( ){
     return(
         <Container>
             <Header/>
+            <Form>
+                <Input type="text" placeholder="Nome do filme"/>
+                <textarea/>
+
+                <Button
+                    title={"Salvar filme"}
+                />
+            </Form>
         </Container>
     )
 }
--- src/pages/CreateMovie/styles.js
+++ src/pages/CreateMovie/styles.js
@@ -2,3 +2,34 @@ import styled from "styled-components";
 
 export const Container = styled.div`
 `
+
+export const Form = styled.form`
+
+    width: 700px;
+    margin: 0 auto;
+    margin-top: 7rem;
+    text-align: center;
+    textarea {
+        margin: 2rem 0;
+        grid-area: TextArea;
+
+        height: 200px;
+        width: 90%;
+        padding: 1rem;
+        border: none;
+        border-radius: .3rem;
+        background-color: ${({ theme}) => theme.COLORS.BACKGROUND_800};
+
+        color: ${({theme }) => theme.COLORS.WHITE};
+        font-family: "Roboto", sans-serif;
+        font-weight: 300;
+        font-size: 1rem;
+
+        outline: none;
+        resize: none;
+
+    :hover{
+        box-shadow: 0px -20px 36px -28px inset ${({ theme }) => theme.COLORS.BACKGROUND_600};
+    }
+    }
+`
