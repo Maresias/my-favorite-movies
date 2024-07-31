d7797a83f68a1d4fa931a90e6f7db2123c236215
AlexandreRCS <ale.maresias@gmail.com>
2024-07-30 22:48:04 -0300

    Update index.jsx

--- src/pages/CreateMovie/index.jsx
+++ src/pages/CreateMovie/index.jsx
@@ -6,15 +6,31 @@ import { Button } from "../../components/Button";
 import { Brand } from "../../components/Brand";
 
 
+import { useState } from "react";
+
 
 export function CreateMovie ( ){
+
+    const [ title, setTitle ] = useState()
+    const [ description, setDescription ] = useState()
+
+    async function handleAddMovie(){
+        
+    }
     return(
         <Container>
             <Header/>
             <Form>
                 <Brand title={"Adicionar Filme"} route={"/"}/>
-                <Input type="text" placeholder="Nome do filme"/>
-                <textarea placeholder="Resumo do filme"/>
+                <Input 
+                    type="text" 
+                    placeholder="Nome do filme"
+                    onChange={ e => setTitle( e.target.value )}
+                />
+                <textarea 
+                    placeholder="Resumo do filme"
+                    onChange={ e => setDescription( e.target.value )}
+                />
 
                 <div>
                     <Button
